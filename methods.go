39bfda31d0422244682d45f8f75e370c3ac156bb
Wusung Peng <wusungpeng@kkbox.com>
2017-05-17 20:21:20 +0800

    feat: method example

---
+++ methods.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type rect struct {
+    width, height int
+}
+
+func (r *rect) area() int {
+    return r.width * r.height
+}
+
+func (r rect) perim() int {
+    return 2*r.width + 2*r.height
+}
+
+func main() {
+    r := rect{width: 10, height: 4}
+    fmt.Println("area:", r.area())
+    fmt.Println("perimc:", r.perim())
+
+    rp := &r
+    fmt.Println("area:", rp.area())
+    fmt.Println("perimc:", rp.perim())
+}
